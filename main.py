f8d4fd5d193027cd8e9a4fe991768d21bc743026
veed <ovidiu.matusa@gmail.com>
2024-03-06 19:58:41 +0200

    db_uri

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ Bootstrap5(app)
 
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",'sqlite:///users.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
