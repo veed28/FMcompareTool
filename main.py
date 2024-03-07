8c74d32fc43f6cb7121db6f88a88b621e4efa43c
veed <ovidiu.matusa@gmail.com>
2024-03-07 20:15:29 +0200

    with ./uploads/

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def home():
 
     if form.validate_on_submit():
         filename = secure_filename(form.file.data.filename)
-        form.file.data.save(f'/opt/render/project/src/src/uploads/{current_user.name}/' + filename)
+        form.file.data.save(f'./uploads/{current_user.name}/' + filename)
         return redirect(url_for('home'))
 
     return render_template("index.html", form=form, logged_in=current_user.is_authenticated)
@@ -90,7 +90,7 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
-        path = '/opt/render/project/src/src/uploads/' + new_user.name
+        path = './uploads/' + new_user.name
         os.mkdir(path)
         return redirect(url_for("home"))
 
@@ -127,7 +127,7 @@ def role(selected_role):
     first_dict = {}
     final_list = []
     user_name = current_user.name
-    file = open(f"/opt/render/project/src/src/uploads/{user_name}/Untitled.rtf", encoding="utf-8")
+    file = open(f"./uploads/{user_name}/Untitled.rtf", encoding="utf-8")
     content = file.read()
     text = rtf_to_text(content)
     text2 = text.replace(" ", "")
